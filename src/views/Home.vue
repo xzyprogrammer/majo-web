11a077cea3059c002183f19b5421d1153fec4a86
Franco Salinas <franco.salinas.at@gmail.com>
2024-10-16 13:04:23 -0300

    Se añade visual inicial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,38 @@
 <template>
-    <div>
-        <h1>Test Home</h1>
+    <div class="navbar">
+        <p>Navbar</p>
+    </div>
+    <div class="main-content">
+        <div class="portada-content">
+            <p>Portada</p>
+        </div>
+        <div class="speakers-content">
+            <p>Speakers</p>
+        </div>
+        <div class="enterprise-content">
+            <p>Empresas</p>
+        </div>
+    </div>
+    <div class="footer">
+        <p>Footer</p>
     </div>
 </template>
 <script>
 
 export default {
-  name: 'Home',
-  data(){
-    
+  data() {
+    return {
+      navbarOptions: [
+        { text: "Home", link: "/", class: "btn-home" },
+        { text: "About", link: "/about", class: "btn-about" },
+        { text: "Contact", link: "/contact", class: "btn-contact" }
+      ]
+    };
   },
   methods: {
-
+    mounted(){
+        console.log(this.navbarOptions);
+    }
   }
 }
 
